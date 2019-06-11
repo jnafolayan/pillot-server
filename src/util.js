f34d9bc9e3fdf07d8e7f95bc868e11ae1690b151
John Afolayan <jnafolayan@gmail.com>
2019-06-11 22:44:14 +0100

    feat: add jwt auth middleware

--- src/util.js
+++ src/util.js
@@ -2,3 +2,21 @@ export function createError(status, msg) {
   const error = new Error(msg);
   error.status = status;
 }
+
+export function verifyAuth(req, res, next) {
+  const tokenBearer = req.headers['Bearer']
+  
+  let token;
+
+  try {
+    token = tokenBearer.split(' ')[1];
+  } catch (error) {
+    return next(error);
+  }
+
+  jwt.verify(token, config.jwtSecret, (err, decoded) => {
+    if (err)
+      return next(err);
+    req.user = { id: decoded.id };
+  });
+}
